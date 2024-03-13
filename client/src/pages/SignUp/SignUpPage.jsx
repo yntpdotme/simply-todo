51414ac2ba9c184123e8320c20d98bdc45c5a988
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-13 12:05:58 +0530

    feat(frontend): implement SignUpPage UI

--- client/src/pages/SignUp/SignUpPage.jsx
+++ client/src/pages/SignUp/SignUpPage.jsx
@@ -1,10 +1,29 @@
+import {Link, useNavigate} from 'react-router-dom';
+
+import {AuthForm} from '@components';
 import './SignUpPage.css';
 
 const SignUpPage = () => {
-	return (
-    <>
-      <div>SignUpPage</div>
-    </>
+  const navigate = useNavigate();
+
+  const handleSignUp = async () => {
+    navigate('/todos');
+  };
+
+  return (
+    <div>
+      <h1>Create Account</h1>
+      <div className="authform">
+        <AuthForm
+          onSubmit={handleSignUp}
+          buttonText="Create my account"
+          isSignUp
+        />
+      </div>
+      <p className="footer">
+        Have an account? <Link to="/signin">Sign In</Link>
+      </p>
+    </div>
   );
 };
 
