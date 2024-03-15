3aab8bbfdc018e19c2ff40b0508b455fdaa952a3
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-15 14:27:43 +0530

    feat(frontend): integrate signup functionality with backend API

--- client/src/pages/SignUp/SignUpPage.jsx
+++ client/src/pages/SignUp/SignUpPage.jsx
@@ -1,18 +1,24 @@
 import {Link, useNavigate} from 'react-router-dom';
 
+import {AuthService, LocalStorage} from '@services';
 import {AuthForm, GuestLogin} from '@components';
 import './SignUpPage.css';
 
 const SignUpPage = () => {
   const navigate = useNavigate();
 
-  const handleSignUp = async () => {
+  const handleSignUp = async formData => {
+    const response = await AuthService.signUp(formData);
+    const {accessToken} = response?.data?.data || {};
+
+    LocalStorage.set('accessToken', accessToken);
+
     navigate('/todos');
   };
 
   return (
     <div>
-      <p className='signup'>Create an account</p>
+      <p className="signup">Create an account</p>
       <div className="authform">
         <AuthForm
           onSubmit={handleSignUp}
